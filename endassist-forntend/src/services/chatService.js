574263ebf6a9587c1c6cb15c509121162ff746b4
Chamath Goonetilleke <31180821+Chamath-Goonetilleke@users.noreply.github.com>
2025-03-16 17:57:42 +0530

    chat

--- endassist-forntend/src/services/chatService.js
+++ endassist-forntend/src/services/chatService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getAnswer(data){
-    return await axios.post("http://127.0.0.1:5000/get", data);
+    return await axios.post("https://endassist-chat-api.vercel.app/get", data);
 }
 
 export async function getRecommendation(data) {
